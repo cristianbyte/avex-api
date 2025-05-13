1fb6dc00ca589d4ddf9071e065f934eeef20a207
cristianbyte <charlsmaritz@gmail.com>
2025-05-12 23:08:20 -0500

    Add Lombok @Builder annotation to Account, Category, Transaction, and User entities

--- src/main/java/com/alex/domain/entity/Category.java
+++ src/main/java/com/alex/domain/entity/Category.java
@@ -11,12 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "categories")
